3a5b463030d0d636539ed4513687c59e578597a3
4-tel <4-tel@protonmail.com>
2024-07-02 18:09:52 +0200

    1. Smoke test

---
+++ contracts/FractalFile.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.5.16;
+
+contract Fractal {
+    // Constructor
+    // Set the total number of tokens
+    //read the total number of tokens
+    uint256 public totalSupply;
+
+    constructor() public {
+        totalSupply = 1000000;
+    }
+}
---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,5 @@
+var Fractal = artifacts.require("Fractal");
+
+module.exports = function (deployer) {
+    deployer.deploy(Fractal);
+};
