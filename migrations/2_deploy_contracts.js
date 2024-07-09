85dd89f2bee9b8e21f3935dafd20ce000733de89
4-tel <4-tel@protonmail.com>
2024-07-09 13:14:13 +0200

    4. Token Sale

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,11 @@
 var Fractal = artifacts.require("Fractal");
-
+var FractalSale = artifacts.require("FractalSale");
 module.exports = function (deployer) {
-    deployer.deploy(Fractal, 1000000);
+    deployer.deploy(Fractal, 1000000).then(function () {
+        // Token price is 0.001 Ether
+        var tokenPrice = 1000000000000000;
+        return deployer.deploy(FractalSale, Fractal.address, tokenPrice);
+
+    });
+
 };
